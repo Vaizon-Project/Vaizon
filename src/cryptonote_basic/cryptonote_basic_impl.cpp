dbd0447a6e35cf5a53b747137114873ac7eff89e
edward-pops <edward@vaizon.com>
2020-04-26 17:20:41 +0530

    reward structure change

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -118,7 +118,7 @@ namespace cryptonote {
     uint64_t base_reward =
       version >= network_version_16 ? BLOCK_REWARD_HF16 :
       version >= network_version_15_lns ? BLOCK_REWARD_HF15 :
-      version >= block_reward_unpenalized_formula_v7(already_generated_coins, height);
+      block_reward_unpenalized_formula_v7(already_generated_coins, height);
 
     uint64_t full_reward_zone = get_min_block_weight(version);
 
